9f363f0eb38f38ba06b982cb117b5b81add7fa05
Jacob Sowles <jacob.sowles@speakcreative.com>
2016-12-24 14:09:05 -0800

    docs: point Sidebar sub-modules to Sidebar demo

--- docs/components/SidebarHeaderDocs.jsx
+++ docs/components/SidebarHeaderDocs.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 
 class SidebarHeaderDocs extends Component {
     render() {
         return (
             <div>
                 <h1>SidebarHeader</h1>
+
                 <h2>Demo</h2>
+                <Link to="/components/sidebar">See the Sidebar demo</Link>
+
                 <h2>Usage</h2>
                 <h2>Example</h2>
             </div>
--- docs/components/SidebarModuleDocs.jsx
+++ docs/components/SidebarModuleDocs.jsx
@@ -1,14 +1,15 @@
-// npm modules
 import React, { Component } from 'react';
-
-// app modules
+import { Link } from 'react-router';
 
 class SidebarModuleDocs extends Component {
     render() {
         return (
             <div>
                 <h1>SidebarModule</h1>
+
                 <h2>Demo</h2>
+                <Link to="/components/sidebar">See the Sidebar demo</Link>
+
                 <h2>Usage</h2>
                 <h2>Example</h2>
             </div>
